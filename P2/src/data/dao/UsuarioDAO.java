2efdfb52ad8ddd1c92b170d4993cf7fc0bf65361
alvarohghg <61513412+alvarohghg@users.noreply.github.com>
2021-10-28 21:03:49 +0200

    Add files via upload

---
+++ P2/src/data/dao/UsuarioDAO.java
@@ -0,0 +1,44 @@
+package data.dao;
+import java.sql.*;
+import java.util.ArrayList;
+
+//import com.mysql.jdbc.ResultSet;
+
+import business.Usuario;
+import data.common.DBConnection;
+
+
+
+public class UsuarioDAO {
+	public ArrayList<Usuario>obtenerUsuarios(){
+		ArrayList<Usuario> listOfUsers = new ArrayList<Usuario>();
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			// Important: This query is hard-coded here for illustrative purposes only
+			String query = "select * from usuario";
+			
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			Statement stmt = connection.createStatement();
+			ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+			while (rs.next()) {
+				String nombre = rs.getString("nombre");
+				String apellidos = rs.getString("apellidos");
+				String tipo = rs.getString("tipo");
+				String correo = rs.getString("correo");
+				String nick = rs.getString("nick");
+				listOfUsers.add(new Usuario(nombre, apellidos, nick,correo,tipo));
+			}
+
+			if (stmt != null){ 
+				stmt.close(); 
+			}
+			dbConnection.closeConnection();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		return listOfUsers;
+	}
+}
