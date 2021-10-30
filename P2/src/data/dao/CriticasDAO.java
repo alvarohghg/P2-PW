b5bda5f2f434a6c68f123968b651ac4d1e7a834b
alvarohghg <61513412+alvarohghg@users.noreply.github.com>
2021-10-30 20:27:35 +0200

    sou 1 put

--- P2/src/data/dao/CriticasDAO.java
+++ P2/src/data/dao/CriticasDAO.java
@@ -9,7 +9,7 @@ import business.Criticas;
 import data.common.DBConnection;
 
 public class CriticasDAO {
-	public ArrayList<Criticas> obtenerUsuarios(){
+	public ArrayList<Criticas> obtenerCriticas(){
 		ArrayList<Criticas> listOfCriticas = new ArrayList<Criticas>();
 		try {
 			DBConnection dbConnection = new DBConnection();
@@ -61,6 +61,89 @@ public class CriticasDAO {
 			String query = "INSERT INTO  `criticas` (`titulo`, `puntuacion`, `espectaculo` , `review`, `valoraciones`,`controlarPrimeraVez`,`autor`,`votantes`) VALUES ( "
 			+ titulo + puntuacion + espectaculo + review + valoraciones +controlarPrimeraVez+autor+votantes+ " )";
 					
+			
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			Statement stmt = connection.createStatement();
+			ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+
+			if (stmt != null){ 
+				stmt.close(); 
+			}
+			dbConnection.closeConnection();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	
+	public void actualizarAutorBD(String correonuevo,String correoviejo){
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query=null; 
+			// Important: This query is hard-coded here for illustrative purposes only
+			query = "UPDATE criticas SET autor = "+correonuevo+" WHERE [Last autor] = " + correoviejo;
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			Statement stmt = connection.createStatement();
+			ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+
+			if (stmt != null){ 
+				stmt.close(); 
+			}
+			dbConnection.closeConnection();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	
+	public void borraCriticaBD(String titulo, String correo){
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query="DELETE FROM criticas WHERE titulo = "+ titulo+"AND autor ="+correo;
+			
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			Statement stmt = connection.createStatement();
+			ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+
+			if (stmt != null){ 
+				stmt.close(); 
+			}
+			dbConnection.closeConnection();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	public void actualizarCriticaBDvotantes(String titulo,String votantes) {
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query = "UPDATE criticas SET votantes = "+votantes+" WHERE titulo = " + titulo;
+			
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			Statement stmt = connection.createStatement();
+			ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+
+			if (stmt != null){ 
+				stmt.close(); 
+			}
+			dbConnection.closeConnection();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	public void actualizarCriticaBDpuntuacion(String titulo,String puntuacion) {
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query = "UPDATE criticas SET puntuacion = "+puntuacion+" WHERE titulo = " + titulo;
 			
 			// Important: We can replace this direct invocation to CRUD operations in DBConnection
 			Statement stmt = connection.createStatement();
