a0ea974fc234d64e4c8cb790cd8bc55be59c9b27
DweOfisial <91595242+DweOfisial@users.noreply.github.com>
2021-10-31 13:01:26 +0100

    Add files via upload

---
+++ P2/src/data/dao/TemporadaDAO.java
@@ -0,0 +1,50 @@
+package data.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import business.*;
+import business.AbstractEspectaculo.categoria;
+import data.common.DBConnection;
+
+public class TemporadaDAO {
+	public ArrayList<EspectaculoTemporada> obtenerPuntual(){
+		ArrayList<EspectaculoTemporada> listaT = new ArrayList<EspectaculoTemporada>();
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			// Important: This query is hard-coded here for illustrative purposes only
+			String query = "select * from espectaculotemporada";
+			
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			Statement stmt = connection.createStatement();
+			ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+			while (rs.next()) {
+				String titulo = rs.getString("titulo_temp");
+				String descripcion = rs.getString("descripcion_temp");
+				categoria cate =business.AbstractEspectaculo.categoria.valueOf(rs.getString("categoria_temp"));
+				int aforo = rs.getInt("aforolocalidades_temp");
+				int localidades = rs.getInt("localidadesvendidas_temp");
+				String dia= rs.getString("dia_temp");
+				Date inicio = rs.getDate("inicio_temp");
+				Date fin = rs.getDate("fin_temp");
+				listaT.add(new EspectaculoTemporada(titulo, descripcion, cate,aforo,localidades,dia,inicio,fin));
+			}
+
+			if (stmt != null){ 
+				stmt.close(); 
+			}
+			dbConnection.closeConnection();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		return listaT;
+	}
+
+}
+
