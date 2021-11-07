273d9c7971fcd6ef70067b858d7f43759693ab38
alvarohghg <61513412+alvarohghg@users.noreply.github.com>
2021-11-07 01:04:53 +0100

    he añadido eliminar  temporada bebed awa

--- P2/src/data/dao/TemporadaDAO.java
+++ P2/src/data/dao/TemporadaDAO.java
@@ -1,5 +1,7 @@
 package data.dao;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
@@ -79,4 +81,96 @@ public void escribirTemporadaBD(EspectaculoTemporada temporada) {
 	}
 	}
 
+public void actualizarTemporadaBD(String titulo,String nuevotitulo,String nuevadescripcion,categoria nuevacategoria,int nuevoaforolocalidades,
+		int localidadesvendidas,Date nuevafechafin,Date nuevafechainicio,String dia,int opcion) {
+	try {
+		DBConnection dbConnection = new DBConnection();
+		Connection connection = dbConnection.getConnection();
+		String query=null; 
+		// Important: This query is hard-coded here for illustrative purposes only
+		switch(opcion) {
+		case 1:
+			query = "UPDATE espectaculotemporada SET titulo_temp = "+ nuevotitulo + "WHERE [Last titulo_temp] = " + titulo;
+		break;
+		case 2:
+			query = "UPDATE espectaculotemporada SET descripcion_temp =  "+ nuevadescripcion + "WHERE [Last titulo_temp] = " + titulo;
+		break;
+		case 3:
+			query = "UPDATE espectaculotemporada SET categoria_temp = "+ nuevacategoria +"WHERE [Last titulo_temp] = " + titulo;
+		break;
+		case 4:
+			query = "UPDATE espectaculotemporada SET aforolocalidades_temp =  "+ String.valueOf(nuevoaforolocalidades) +"WHERE [Last titulo_temp] = " + titulo;
+		break;
+		case 5:
+			query = "UPDATE espectaculotemporada SET localidadesvendidas_temp = "+ String.valueOf(localidadesvendidas) +"WHERE [Last titulo_temp] = " + titulo;
+		break;
+		case 6:
+			query = "UPDATE espectaculotemporada SET dia_temp =  "+ String.valueOf(dia) +"WHERE [Last titulo_temp] = " + titulo;
+		break;
+		case 7:
+			query = "UPDATE espectaculotemporada SET inicio_temp =  "+ String.valueOf(nuevafechainicio) +"WHERE [Last titulo_temp] = " + titulo;
+		break;
+		case 8:
+			query = "UPDATE espectaculotemporada SET fin_temp =  "+ String.valueOf(nuevafechafin) +"WHERE [Last titulo_temp] = " + titulo;
+		break;
+		}
+		
+				
+		
+		// Important: We can replace this direct invocation to CRUD operations in DBConnection
+		Statement stmt = connection.createStatement();
+		ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+
+		if (stmt != null){ 
+			stmt.close(); 
+		}
+		dbConnection.closeConnection();
+	} catch (Exception e){
+		System.err.println(e);
+		e.printStackTrace();
+	}
+}
+public void eliminarTemporadaTitulo(String titulo){
+	try {
+		DBConnection dbConnection = new DBConnection();
+		Connection connection = dbConnection.getConnection();
+		String query="DELETE FROM espectaculotemporada WHERE titulo_temp = "+ titulo;
+		
+		// Important: We can replace this direct invocation to CRUD operations in DBConnection
+		Statement stmt = connection.createStatement();
+		ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+
+		if (stmt != null){ 
+			stmt.close(); 
+		}
+		dbConnection.closeConnection();
+	} catch (Exception e){
+		System.err.println(e);
+		e.printStackTrace();
+	}
+}
+//QUE HACEMOS PARA ELIMINAR ESPECTACULOS CUYA FECHA QUE LE PASES POR ESTA FUNCION ESTÉ EN MEDIO?¿?¿
+public void eliminarTemporadaFecha(String titulo, Date fecha){
+	try {
+		DBConnection dbConnection = new DBConnection();
+		Connection connection = dbConnection.getConnection();
+		String query = "DELETE FROM espectaculotemporada WHERE titulo_temp = "+ titulo + "AND inicio_temp ='"+ fecha +"' OR fin_temp ='"+fecha
+				+"' OR( '"+fecha+"' >=inicio_temp AND '"+fecha+"'<=fin_temp)";
+		
+		// Important: We can replace this direct invocation to CRUD operations in DBConnection
+		Statement stmt = connection.createStatement();
+		ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+
+		if (stmt != null){ 
+			stmt.close(); 
+		}
+		dbConnection.closeConnection();
+	} catch (Exception e){
+		System.err.println(e);
+		e.printStackTrace();
+	}
+}
 }
