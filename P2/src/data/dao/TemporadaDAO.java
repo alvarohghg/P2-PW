994b267115a59ef58220dce28e8a077aa1259cc4
DweOfisial <91595242+DweOfisial@users.noreply.github.com>
2021-11-09 17:41:08 +0100

    Update TemporadaDAO.java

--- P2/src/data/dao/TemporadaDAO.java
+++ P2/src/data/dao/TemporadaDAO.java
@@ -1,10 +1,10 @@
 package data.dao;
 import java.io.*;
 import java.io.BufferedReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -68,7 +68,6 @@ public class TemporadaDAO {
         }
         return f;
     }
-	
 	public ArrayList<EspectaculoTemporada> obtenerTemporada(){
 		ArrayList<EspectaculoTemporada> listaT = new ArrayList<EspectaculoTemporada>();
 		try {
@@ -107,6 +106,8 @@ public void escribirTemporadaBD(EspectaculoTemporada temporada) {
 	try {
 		DBConnection dbConnection = new DBConnection();
 		Connection connection = dbConnection.getConnection();
+		String query =propiedades(2);
+		PreparedStatement ps=connection.prepareStatement(query);
 		// Important: This query is hard-coded here for illustrative purposes only
 		String titulo_temp=temporada.getTitulo();
 		String descripcion_temp=temporada.getDescripcion();
@@ -116,17 +117,18 @@ public void escribirTemporadaBD(EspectaculoTemporada temporada) {
 		String dia_temp=temporada.getDia();
 		String inicio_temp=String.valueOf(temporada.getInicio());
 		String fin_temp=String.valueOf(temporada.getFin());
-		String query =propiedades(2);
+		ps.setString(1, titulo_temp);
+		ps.setString(2, descripcion_temp);
+		ps.setString(3, categoria_temp);
+		ps.setString(4, aforolocalidades_temp);
+		ps.setString(5, localidadesvendidas_temp);
+		ps.setString(6, dia_temp);
+		ps.setString(7, inicio_temp);
+		ps.setString(8, fin_temp);
+		ps.executeUpdate();
+		
 				
 		
-		// Important: We can replace this direct invocation to CRUD operations in DBConnection
-		Statement stmt = connection.createStatement();
-		ResultSet rs = (ResultSet) stmt.executeQuery(query);
-
-
-		if (stmt != null){ 
-			stmt.close(); 
-		}
 		dbConnection.closeConnection();
 	} catch (Exception e){
 		System.err.println(e);
@@ -135,7 +137,7 @@ public void escribirTemporadaBD(EspectaculoTemporada temporada) {
 	}
 
 public void actualizarTemporadaBD(String titulo,String nuevotitulo,String nuevadescripcion,categoria nuevacategoria,int nuevoaforolocalidades,
-		int localidadesvendidas,Date nuevafechafin,Date nuevafechainicio,String dia,int opcion) {
+		int nuevalocalidadesvendidas,Date nuevafechafin,Date nuevafechainicio,String dia,int opcion) {
 	try {
 		DBConnection dbConnection = new DBConnection();
 		Connection connection = dbConnection.getConnection();
@@ -144,40 +146,64 @@ public void actualizarTemporadaBD(String titulo,String nuevotitulo,String nuevad
 		switch(opcion) {
 		case 1:
 			query = propiedades(3);
+			PreparedStatement ps=connection.prepareStatement(query);
+			ps.setString(1, nuevotitulo);
+			ps.setString(2, titulo);
+			ps.executeUpdate();
 		break;
 		case 2:
-			query = propiedades(4);		
+			query = propiedades(4);
+			PreparedStatement ps1=connection.prepareStatement(query);
+			ps1.setString(1, nuevadescripcion);
+			ps1.setString(2, titulo);
+			ps1.executeUpdate();
 		break;
 		case 3:
-			query = propiedades(5);		
+			query = propiedades(5);
+			PreparedStatement ps2=connection.prepareStatement(query);
+			ps2.setString(1, nuevacategoria.toString());
+			ps2.setString(2, titulo);
+			ps2.executeUpdate();
 		break;
 		case 4:
-			query = propiedades(6);		
+			query = propiedades(6);
+			PreparedStatement ps3=connection.prepareStatement(query);
+			ps3.setInt(1, nuevoaforolocalidades);
+			ps3.setString(2, titulo);
+			ps3.executeUpdate();
 		break;
 		case 5:
-			query = propiedades(7);		
+			query = propiedades(7);
+			PreparedStatement ps4=connection.prepareStatement(query);
+			ps4.setInt(1, nuevalocalidadesvendidas);
+			ps4.setString(2, titulo);
+			ps4.executeUpdate();
 		break;
 		case 6:
-			query = propiedades(8);		
+			query = propiedades(8);
+			PreparedStatement ps5=connection.prepareStatement(query);
+			ps5.setString(1, dia);
+			ps5.setString(2, titulo);
+			ps5.executeUpdate();
 			break;
 		case 7:
-			query = propiedades(9);		
+			query = propiedades(9);	
+			PreparedStatement ps6=connection.prepareStatement(query);
+			ps6.setDate(1, nuevafechainicio);
+			ps6.setString(2, titulo);
+			ps6.executeUpdate();
 			break;
 		case 8:
-			query = propiedades(10);		
+			query = propiedades(10);
+			PreparedStatement ps7=connection.prepareStatement(query);
+			ps7.setDate(1, nuevafechafin);
+			ps7.setString(2, titulo);
+			ps7.executeUpdate();
 			break;
 		}
 		
 				
-		
-		// Important: We can replace this direct invocation to CRUD operations in DBConnection
-		Statement stmt = connection.createStatement();
-		ResultSet rs = (ResultSet) stmt.executeQuery(query);
-
-
-		if (stmt != null){ 
-			stmt.close(); 
-		}
+			
 		dbConnection.closeConnection();
 	} catch (Exception e){
 		System.err.println(e);
@@ -191,36 +217,29 @@ public void eliminarTemporadaTitulo(String titulo){
 		String query= propiedades(11);		
 		
 		
-		// Important: We can replace this direct invocation to CRUD operations in DBConnection
-		Statement stmt = connection.createStatement();
-		ResultSet rs = (ResultSet) stmt.executeQuery(query);
+		PreparedStatement ps=connection.prepareStatement(query);
+		ps.setString(1, titulo);
+		ps.executeUpdate();
 
-
-		if (stmt != null){ 
-			stmt.close(); 
-		}
-		dbConnection.closeConnection();
 	} catch (Exception e){
 		System.err.println(e);
 		e.printStackTrace();
 	}
 }
-//QUE HACEMOS PARA ELIMINAR ESPECTACULOS CUYA FECHA QUE LE PASES POR ESTA FUNCION EST� EN MEDIO?�?�
+//QUE HACEMOS PARA ELIMINAR ESPECTACULOS CUYA FECHA QUE LE PASES POR ESTA FUNCION ESTÉ EN MEDIO?¿?¿
 public void eliminarTemporadaFecha(String titulo, Date fecha){
 	try {
 		DBConnection dbConnection = new DBConnection();
 		Connection connection = dbConnection.getConnection();
 		String query = propiedades(12);
+		PreparedStatement ps=connection.prepareStatement(query);
+		ps.setString(1, titulo);
+		ps.setDate(2, fecha);
+		ps.setDate(3, fecha);
+		ps.setDate(4, fecha);
+		ps.setDate(5, fecha);
+		ps.executeUpdate();
 		
-		// Important: We can replace this direct invocation to CRUD operations in DBConnection
-		Statement stmt = connection.createStatement();
-		ResultSet rs = (ResultSet) stmt.executeQuery(query);
-
-
-		if (stmt != null){ 
-			stmt.close(); 
-		}
-		dbConnection.closeConnection();
 	} catch (Exception e){
 		System.err.println(e);
 		e.printStackTrace();
