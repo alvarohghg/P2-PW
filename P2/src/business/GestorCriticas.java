26c00ca4bfe745dba7ae6ea315bc510aa3305523
DweOfisial <91595242+DweOfisial@users.noreply.github.com>
2021-11-09 22:47:28 +0100

    Update GestorCriticas.java

--- P2/src/business/GestorCriticas.java
+++ P2/src/business/GestorCriticas.java
@@ -66,14 +66,13 @@ public class GestorCriticas {
 	 * @throws IOException
 	 */
 	public boolean crearCritica(String titulo,String espectaculo,String puntuacion,String review, String correo, String valoraciones)throws IOException{
-		leerFicheroUsuarios();
+		ListaCriticas= CDAO.obtenerCriticas();
 		boolean correcto=false;
 		for(int i=0; i< ListaUsuario.size(); i++){
 			if(correo.equals(ListaUsuario.get(i).getCorreo())){
 					Criticas C1 = new Criticas(titulo,espectaculo,puntuacion,review,correo,valoraciones,"0","");
 					subirCritica(C1);
-					correcto=true;
-				
+					correcto=true;				
 			}
 		}
 		
@@ -97,7 +96,7 @@ public class GestorCriticas {
 	 * @throws IOException
 	 */
 	public ArrayList<Criticas> verCriticas() throws IOException {
-		leerFicheroUsuarios();
+		ListaCriticas=CDAO.obtenerCriticas();
 		return ListaCriticas;
 	}
 	
@@ -178,142 +177,6 @@ public class GestorCriticas {
 	}
 	
 	
-		
-	public String propiedades(int r) {
-		
-		Properties prop = new Properties();
-		String filename = "conf.propierties";
-		String f=null;
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
-			prop.load(reader);
-			if(r==0) {
-				f = prop.getProperty("criticas");
-			}
-			else {
-				f = prop.getProperty("usuario");
-			}
-			//System.out.println(f);			
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return f;
-	}
-	
-	/**
-	 * Nos dan una critica y la escribimos en el fichero correspondiente 
-	 * @param C Critica a escribir en el fichero
-	 */
-	/*
-	public void escribirFicheroCritica(Criticas C){
-		FileWriter fichero = null;
-		String f=null;
-		f=propiedades(0);
-		//System.out.println(f);
-		try {
-
-			fichero = new FileWriter(f, true);
-			//fichero = new FileWriter(f);
-			
-			fichero.write(C.getTitulo()+"\n");
-			fichero.write(C.getEspectaculo()+"\n");
-			fichero.write(C.getPuntuacion()+"\n");
-			fichero.write(C.getReview()+"\n");
-			fichero.write(C.getAutor()+"\n");
-			fichero.write(C.getValoraciones()+"\n");
-			fichero.write(C.getcontrolarPrimeraVez()+"\n");			
-			fichero.write(C.getVotantes()+"\n");
-			
-			//fichero.write("\n");
-			fichero.close();
-
-		} catch (Exception ex) {
-			System.out.println("Mensaje de la excepci�n: " + ex.getMessage());
-		}
-	}
-	*/
-	/**
-	 * Leemos el fichero de criticas y guardamos su contenido en nuestra lista de criticas para poder trabajar con ella
-	 * @throws IOException
-	 */
-	/*
-	public void leerFicheroCriticas() throws IOException {
-		BufferedReader in = null;
-		ArrayList<Criticas> lista = new ArrayList<Criticas>();
-		ArrayList<String>  vec = new ArrayList<String>();
-		File file = new File("ficheroC.txt");
-        file.createNewFile();
-       
-		String f=null;
-		f=propiedades(0);
-		//System.out.println(f);
-		
-          
-
-		try {   
-		    in = new BufferedReader(new FileReader(f));
-		    String str;
-		    while ((str = in.readLine()) != null) {
-		        vec.add(str);
-		        
-		    }
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} finally {
-		    if (in != null) {
-		        //in.close();
-		    }
-		}
-		
-		for(int ind=0;ind<vec.size();ind+=8) {
-				Criticas aux=new Criticas(vec.get(ind),vec.get(ind+1),vec.get(ind+2),vec.get(ind+3),vec.get(ind+4),vec.get(ind+5),vec.get(ind+6),vec.get(ind+7));
-				lista.add(aux);
-		}
-			
-		
-		ListaCriticas=lista;
-		
-	}
-		
-	*/
-	/**
-	 * Leemos el fichero de usuarios y lo guardamos en nuestra lista de usuarios para poder trabajar en ella
-	 * @throws IOException
-	 */
-	
-	public void leerFicheroUsuarios()throws IOException  {
-		BufferedReader in = null;
-		ArrayList<String>  vec = new ArrayList<String>();
-		File file = new File("ficheroU.txt");
-        file.createNewFile();
-		String f=null;
-		f=propiedades(1);
-		try {   
-			in = new BufferedReader(new FileReader(f));
-		    String str;
-		    while ((str = in.readLine()) != null) {
-		        vec.add(str);
-		        
-		    }
-		} catch (FileNotFoundException e) {
-		    e.printStackTrace();
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
-		
-		for(int ind=0;ind<vec.size();ind+=5) {
-				Usuario aux=new Usuario(vec.get(ind+1),vec.get(ind+2),vec.get(ind+3),vec.get(ind),vec.get(ind+4));
-				ListaUsuario.add(aux);
-		}
-		
-	}
-		
 	/**
 	 * Actualizamos las criticas de autor cuando este se cambia el correo
 	 * @param correoNuevo Correo nuevo
