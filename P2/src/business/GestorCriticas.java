9cf286f79b9ee29a514e405dc11cfa1d0be1a798
DweOfisial <91595242+DweOfisial@users.noreply.github.com>
2021-11-10 21:01:23 +0100

    Update GestorCriticas.java

--- P2/src/business/GestorCriticas.java
+++ P2/src/business/GestorCriticas.java
@@ -1,202 +1,209 @@
-package business;
+package data.dao;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
-import java.util.Scanner;
-import java.io.*;
-import data.dao.CriticasDAO;
-import data.dao.UsuarioDAO;
 
-/**
- * Una clase que representa al gestor de criticas
- * @author Alvaro Berjillos
- * @author Alvaro Sanchez
- * @author Francisco Javier D�az
- * @version 1.0
- * */
+import business.Criticas;
+import data.common.DBConnection;
 
-public class GestorCriticas {
-	/* Atributos */
-	CriticasDAO CDAO=new CriticasDAO();
-	UsuarioDAO UDAO=new UsuarioDAO();
-	private ArrayList<Criticas> ListaCriticas = new ArrayList<Criticas>();
-	Scanner entrada = new Scanner(System.in);
-	private ArrayList<Usuario> ListaUsuario  = new ArrayList<Usuario>();
-	GestorEspectaculos GE=new GestorEspectaculos();
-	//GestorUsuario GU=new GestorUsuario();
-	
-	/* Patron de diseno singlenton */
-	private static GestorCriticas instancia;
-	
-	private GestorCriticas(){}
-	
-	public static GestorCriticas getInstancia(){
-		if(instancia==null) {
-			instancia=new GestorCriticas();
-		}
-		return instancia;
-	}
-	
-	/**
-	 * Comprueba si existe el titulo dado 
-	 * @param titulo El titulo a buscar
-	 * @return true o false segun si exite o no existe el titulo
-	 * */
-	
-	public boolean existeTitulo(String titulo) {
-		boolean existe=false;
-		for(int i=0;i<ListaCriticas.size();i++) {
-			if(titulo.equals(ListaCriticas.get(i).getTitulo())) {	
-				return true;
-			}
-		} 
-		return existe;
-	}
-	
-	public void guardarBDC() {
-		ListaCriticas = CDAO.obtenerCriticas();
-	}
-	
-	/**
-	 * 
-	 * @param titulo El titulo de la critica
-	 * @param espectaculo El espectaculo al que hace la critica
-	 * @param puntuacion La puntuacion que le da al espectaculo
-	 * @param review Su opinion sobre el espectaculo
-	 * @param autor El autor de la critica
-	 * @param valoraciones La puntuacion que le dan los usuario a esa critica
-	 * @throws IOException
-	 */
-	public boolean crearCritica(String titulo,String espectaculo,String puntuacion,String review, String correo, String valoraciones)throws IOException{
-		ListaUsuario = UDAO.obtenerUsuarios();
-		ListaCriticas= CDAO.obtenerCriticas();
-		boolean correcto=false;
-		for(int i=0; i< ListaUsuario.size(); i++){
-			if(correo.equals(ListaUsuario.get(i).getCorreo())){
-					Criticas C1 = new Criticas(titulo,espectaculo,puntuacion,review,correo,valoraciones,"0","");
-					correcto=true;				
-					subirCritica(C1);
-			}
-		}
-		
-		return correcto;
-	}	
-	
-	
-	/**
-	 * Guarda la critica que le pasamos en la lista de critica y en el fichero
-	 * @param c Critca que queremos guardar
-	 * @throws IOException
-	 */
-	public void subirCritica(Criticas c)throws IOException {
-		this.ListaCriticas.add(c);
-		CDAO.escribirCriticasBD(c);
-		
-	}
-	/**
-	 * Devolvemos la lista de criticas para poder interactuar con ella en el main
-	 * @return Devuelve la lista de criticas entera
-	 * @throws IOException
-	 */
-	public ArrayList<Criticas> verCriticas() throws IOException {
-		ListaCriticas=CDAO.obtenerCriticas();
-		return ListaCriticas;
-	}
-	
-	/**
-	 * Borramos la critica que nos dan, comprobando que sea el autor
-	 * @param titulo Titulo de la critica a borrar
-	 * @param correo Correo del usuario que ha iniciado sesion, que tiene que coincidir con el del autor de la critica a borrar
-	 * @throws IOException
-	 */
-	public void borraCritica(String titulo, String correo) throws IOException {
-		
-		//String titulo;
-		for(int i=0; i< ListaCriticas.size(); i++) {
-			if(correo.equals(ListaCriticas.get(i).getAutor()) && titulo.equals(ListaCriticas.get(i).getTitulo())){
-				ListaCriticas.remove(i);
-				CDAO.borraCriticaBD(titulo,correo);
-			}
-		}
-		
-		
-	}
-	/**
-	 * Actualizamos el valor de la critica haciendo la media entre el valor que ya teniamos y el nuevo que nos dan
-	 * @param correo Correo del que ha iniciado sesion, que tiene que ser diferente al del autor de la critica
-	 * @param puntuacion Puntuacion que le da a la critica
-	 * @param titulo titulo de la critica a votar
-	 * @throws IOException
-	 */
-	public void votarCritica(String correo, float puntuacion, String titulo) throws IOException {
-		if(titulo.equals(null)) {
-			System.out.println("ha ocurrido un fallo");
+public class CriticasDAO {
+	public ArrayList<Criticas> obtenerCriticas(){
+		ArrayList<Criticas> listOfCriticas = new ArrayList<Criticas>();
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			// Important: This query is hard-coded here for illustrative purposes only
+			String query = propiedades(1);
 			
-		}else {
-			ArrayList<String> listaVotantes= new ArrayList<String>();
-			String vot=null;
-			for(int i=0; i< ListaCriticas.size(); i++) {
-				if(titulo.equals(ListaCriticas.get(i).getTitulo())){
-					 vot=ListaCriticas.get(i).getVotantes();
-				}
-			}
-			
-			String[] parts = vot.split(",");
-			for(int i=0;i<parts.length;i++) {
-				listaVotantes.add(parts[i]);
-			}
-			
-			boolean yaVoto=false;
-			for(int j=0; j< listaVotantes.size(); j++) {	
-				if(listaVotantes.get(j).equals(correo)) {
-					yaVoto=true;
-				}
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			Statement stmt = connection.createStatement();
+			ResultSet rs = (ResultSet) stmt.executeQuery(query);
+
+			while (rs.next()) {
+				String titulo = rs.getString("titulo");
+				String puntuacion = rs.getString("puntuacion");
+				String espectaculo = rs.getString("espectaculo");
+				String review = rs.getString("review");
+				String valoraciones = rs.getString("valoraciones");
+				String controlarPrimeraVez=rs.getString("controlarPrimeraVez");
+				String autor = rs.getString("autor");
+				String votantes = rs.getString("votantes");
+				listOfCriticas.add(new Criticas(titulo, espectaculo, puntuacion,review,autor,valoraciones,controlarPrimeraVez,votantes));
 			}
 
-			if(yaVoto==false) {
-				for(int i=0; i< ListaCriticas.size(); i++) {			
-					if(titulo.equals(ListaCriticas.get(i).getTitulo())) {
-						float estrellas= Float.parseFloat(ListaCriticas.get(i).getValoraciones());
-						if(ListaCriticas.get(i).getcontrolarPrimeraVez().equals("0")) {
-							puntuacion=puntuacion*2;
-							ListaCriticas.get(i).setcontrolarPrimeraVez("1");
-						}
-						String op=Float.toString((puntuacion+estrellas)/2);
-						ListaCriticas.get(i).setValoraciones(op);
-						CDAO.actualizarCriticaBDpuntuacion(titulo, op);
-						ListaCriticas.get(i).setVotantes(ListaCriticas.get(i).getVotantes()+correo+",");
-						CDAO.actualizarCriticaBDvotantes(titulo,ListaCriticas.get(i).getVotantes()+correo+"," );
-					}
-				}
+			if (stmt != null){ 
+				stmt.close(); 
 			}
-			else {
-				System.out.println("No puede votar 2 veces la misma critica\n");
-			}
-			
-			
+			dbConnection.closeConnection();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
 		}
-		
-		
+		return listOfCriticas;
 	}
 	
-	
-	/**
-	 * Actualizamos las criticas de autor cuando este se cambia el correo
-	 * @param correoNuevo Correo nuevo
-	 * @param correoViejo Correo viejo
-	 * @throws IOException
-	 */
-	
-	public void actualizarAutor(String correoNuevo, String correoViejo) throws IOException {
-		for(int i=0;i<ListaCriticas.size();i++) {
-			if(correoViejo.equals(ListaCriticas.get(i).getAutor())) {
-				ListaCriticas.get(i).setAutor(correoNuevo);
-			}
+	public void escribirCriticasBD(Criticas critica) {
+		
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			// Important: This query is hard-coded here for illustrative purposes only
+			String query = propiedades(2);
+			PreparedStatement ps=connection.prepareStatement(query);
+			
+			String titulo=critica.getTitulo();
+			String puntuacion=critica.getPuntuacion();
+			String espectaculo=critica.getEspectaculo();
+			String review=critica.getReview();
+			String valoraciones=critica.getValoraciones();
+			String controlarPrimeraVez=critica.getcontrolarPrimeraVez();
+			String autor=critica.getAutor();
+			String votantes=critica.getVotantes();
+			
+			ps.setString(1,titulo);
+			ps.setString(2,puntuacion);
+			ps.setString(3,espectaculo);
+			ps.setString(4,review);
+			ps.setString(5,valoraciones);
+			ps.setString(6,controlarPrimeraVez);
+			ps.setString(7,autor);
+			ps.setString(8,votantes);
+			ps.executeUpdate();
+			
+			
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
 		}
+	}
+	
+	public void actualizarAutorBD(String correonuevo,String correoviejo){
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query=propiedades(3); 
+			
+			PreparedStatement ps=connection.prepareStatement(query);
+			ps.setString(1,correonuevo);
+			ps.setString(2,correoviejo);
+			ps.executeUpdate();
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	
+	public void borraCriticaBD(String titulo, String correo){
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query=propiedades(4);
+			
+			// Important: We can replace this direct invocation to CRUD operations in DBConnection
+			PreparedStatement ps=connection.prepareStatement(query);
 
-		
-				
-			CDAO.actualizarAutorBD(correoNuevo, correoViejo);
-		
+			ps.setString(1,titulo);
+			ps.setString(2,correo);
+			ps.executeUpdate();
+
+			
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
 	}
+	public void actualizarCriticaBDvotantes(String titulo,String votantes) {
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query = propiedades(5);
+			PreparedStatement ps=connection.prepareStatement(query);
+			ps.setString(1,votantes);
+			ps.setString(2,titulo);
+			ps.executeUpdate();
+			
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	public void actualizarCriticaBDpuntuacion(String titulo,String puntuacion) {
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query = propiedades(6);
+			PreparedStatement ps=connection.prepareStatement(query);
+			ps.setString(1,puntuacion);
+			ps.setString(2,titulo);
+			ps.executeUpdate();
+			
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	
+	public void primeraVez(String titulo) {
+		try {
+			DBConnection dbConnection = new DBConnection();
+			Connection connection = dbConnection.getConnection();
+			String query = propiedades(7);
+			PreparedStatement ps=connection.prepareStatement(query);
+			ps.setString(1,titulo);
+			ps.executeUpdate();
+			
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+	}
+	
+	public String propiedades(int r) {
+        Properties prop = new Properties();
+        String filename = "sqlC.properties";
+        String f=null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+            prop.load(reader);
+            if(r==1) {
+                f = prop.getProperty("obtenerCriticas");
+            }
+            else if(r==2) {
+                f = prop.getProperty("escribirCriticasBD");
+            }
+            else if(r==3) {
+                f = prop.getProperty("actualizarAutor");
+            }
+            else if(r==4) {
+                f = prop.getProperty("borrarCriticaBD");
+            }
+            else if(r==5) {
+                f = prop.getProperty("actualizarCriticaBDvotantes");
+            }
+            else if (r==6){
+                f = prop.getProperty("actualizarCriticaBDpuntuacion");
+            }
+            else {
+                f = prop.getProperty("primeraVez");
+            }
+            //System.out.println(f);            
+        } catch (FileNotFoundException e) {
+            
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return f;
+    }
 	
 }
